2e9fd3717a523dd0b75f582918496bf9f500c478
Vuyaa <st_im@hotmail.com>
2022-11-21 22:02:41 +0100

    Revert "Fixed syntax error"
    This reverts commit 043dc578ae178294a29e28b30776b0b3a32e650d.

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
+import React from "react";
+import  ReactDOM  from "react-dom";
 
-
-// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-// Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-  render() {
-    return (
-      <MainView />
-    );
-  }
+    render() {
+        return (
+            <div className ="my-flix">
+                <div>Good morning</div>
+            </div>
+        );
+    }
 }
 
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
