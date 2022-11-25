9b5a0315f0a4af46e2036a93e587c5e6b35285de
Vuyaa <st_im@hotmail.com>
2022-11-25 13:05:12 +0100

    Linked main view component

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
+import { MainView } from './components/main-view/main-view';
+import { MovieCard } from './components/movie-card/movie-card';
+
 import './index.scss';
+
 
 class MyFlixApplication extends React.Component {
     render() {
-        return (
-            <div className ="my-flix">
-                <div>Good morning</div>
-            </div>
-        );
+      return (
+          <MainView />
+  
+      );
     }
-}
+  }
 
 const container = document.getElementsByClassName('app-container')[0];
 
