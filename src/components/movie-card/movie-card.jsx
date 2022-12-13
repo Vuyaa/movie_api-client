19a0054f892b3137361b7182a2f847cc9c5bc9c4
Vuyaa <st_im@hotmail.com>
2022-12-13 23:20:15 +0100

    Added routes to switch between views

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+<<<<<<< Updated upstream
 import './movie-card.scss';
+=======
+import axios from 'axios';
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import { Link } from "react-router-dom";
+>>>>>>> Stashed changes
+
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
   render() {
+<<<<<<< Updated upstream
     const { movie, onMovieClick } = this.props;
 
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+=======
+    const { movie } = this.props;
+
+    return (
+      <Card className='mb-4'>
+        <Card.Img height="374px" variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text className='text-truncate mb-2'>{movie.Description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button className='mb-1' variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+>>>>>>> Stashed changes
   }
 }
-
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired
   }).isRequired,
+<<<<<<< Updated upstream
   onMovieClick: PropTypes.func.isRequired
 };
+=======
+  onMovieClick: PropTypes.func
+};
+>>>>>>> Stashed changes
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+<<<<<<< Updated upstream
+=======
+import { Row, Col, Button, Image } from "react-bootstrap";
+
+
+>>>>>>> Stashed changes
 import './movie-view.scss';
 
-export class MovieView extends React.Component {
+import { Link } from "react-router-dom";
 
+export class MovieView extends React.Component {
   render() {
+<<<<<<< Updated upstream
     const { movie, onBackClick } = this.props;
 
     return (
@@ -27,6 +35,49 @@ export class MovieView extends React.Component {
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
+=======
+    const { movie, onBackClick, handleFavorite } = this.props;
+
+    return (
+      <Row  >            
+      <Col className="justify-content-md-center">
+        <div className="movie-view">
+          <div className="movie-poster">
+            <img height="800px" width="400px" className='img-fluid shadow-10' src={movie.ImagePath} crossOrigin="Anonymous" />
+          </div>
+          <div className="movie-title">
+            <span className="label">Title:</span>
+            <span className="value">{movie.Title}</span>
+          </div>
+          <div className="movie-description">
+            <span className="label">Description:</span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <div className="movie-genre">
+            <span className="label">Genre:</span>
+            <span className="value">{movie.Genre.Name}</span>
+          </div>
+          
+          <Button
+            className="favorite-button mt-2"
+            variant="primary"
+            onClick={() => handleFavorite(movie._id, "add")}
+          >
+            Add to favorite Movies
+          </Button>
+            <Button
+              className="back-button mt-2"
+              variant="secondary"
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
+           </div> 
+        </Col>
+      </Row>
+>>>>>>> Stashed changes
     );
   }
 }
@@ -41,6 +92,7 @@ MovieView.propTypes = {
     }).isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
+<<<<<<< Updated upstream
       Bio: PropTypes.string.isRequired,
       Birth: PropTypes.string.isRequired,
       Death: PropTypes.string,
@@ -48,3 +100,12 @@ MovieView.propTypes = {
   }).isRequired,
   onBackClick: PropTypes.func.isRequired,
 };
+=======
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    }).isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func
+};
+>>>>>>> Stashed changes
